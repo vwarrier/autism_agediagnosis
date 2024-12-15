25801479aa98c6cdde7d0680a303e1e4b01ec347
Varun Warrier <vw260@medschl.cam.ac.uk>
2024-12-15 08:40:37 +0000

    Update genetic_cor.R

--- gSEM/genetic_cor.R
+++ gSEM/genetic_cor.R
@@ -1,68 +1,58 @@
-#GenomicSEM_basedgeneticcor
-
+library(genomicSEM)
 library(data.table)
-library(GenomicSEM)
 
-#Genetic correlation - genomicSEM
 
-traits <- c("Autism_PGC2_2017.sumstats.gz",  "SPARK_matoba_multiancestry.sumstats.gz", "autism_ipsych_under11.sumstats.gz", "autism_ipsych_over10.sumstats.gz", "Finngen_r10_autism.sumstats.gz", "leeea2018.sumstats.gz")
-sample.prev <- c(0.47, 0.5, 0.2, 0.18, 0.002, NA)
-population.prev <- c(0.027, 0.027, 0.027, 0.027, 0.027, NA)
+
+setwd("/mnt/beegfs/home4/arc/vw260/ldsc/sumstats")
+traits <- c("Autism_PGC2_2017.sumstats.gz", "Autism_iPSYCH_under9.sumstats.gz", "autism_ipsych_over10.sumstats.gz", "Finngen_r10_autism.sumstats.gz",  "SPARK_under6.sumstats", "SPARK_over10.sumstats", "Menarche.sumstats")
+sample.prev <- c(NA, NA, NA, NA, NA, NA, NA)
+population.prev <- c(NA, NA, NA, NA, NA, NA, NA)
 ld <- "~/ldsc/eur_w_ld_chr/"
 wld <- "~/ldsc/eur_w_ld_chr/"
-trait.names<-c("PGC", "SPARK", "Under11", "Over10", "FinnGen", "T")
+trait.names<-c("PGC",  "iPSYCH_under9", "iPSYCH_over10", "FinnGen",  "SPARK_under6",  "SPARK_over10", "T")
 LDSCoutput <- ldsc(traits, sample.prev, population.prev, ld, wld, trait.names)
-##optional command to save the ldsc output in case you want to use it in a later R session. 
 
 
-modelrg<-'ED=~NA*PGC + SPARK + Under11
-  LD=~NA*Over10 + Under11 + FinnGen
+
+modelrg<-'ED =~ NA*PGC + SPARK_under6 + iPSYCH_under9 
+            LD =~ NA*iPSYCH_over10  + FinnGen + SPARK_over10 + iPSYCH_under9
+            ED~~LD
+            iPSYCH_over10 ~~ b*iPSYCH_over10
+            b > .001
+            PGC ~~ a*PGC
+            a > .001
   LT=~NA*T
   LD~~1*LD
   ED~~1*ED
   LT~~1*LT
   T~~0*T
-  ED~~LD
   LD~~0*T
   ED~~0*T'
 
 Autism_CFA<-usermodel(LDSCoutput, estimation = "DWLS", model = modelrg, CFIcalc = TRUE, std.lv = TRUE, imp_cov = FALSE)
 
-Autism_CFA
 
-setwd("/mnt/beegfs/home4/arc/vw260/ldsc")
+ED = Autism_CFA$results[16,] 
 
-#Genetic correlation - GWAS by subtraction
-
-traits <- c("./sumstats/autism_ipsych_over10.sumstats.gz","./sumstats/PTSD_eur_2019.sumstats","./sumstats/anorexia2019.sumstats.gz")
-sample.prev <- c(NA,NA,NA)
-population.prev <- c(NA,NA,NA)
-ld<-"eur_w_ld_chr/"
-wld <- "eur_w_ld_chr/"
-trait.names<-c("Autism", "ADHD", "T")
-
-LDSCoutput <- ldsc(traits, 
-                   sample.prev, 
-                   population.prev, 
-                   ld, 
-                   wld, 
-                   trait.names)
+LD = Autism_CFA$results[17,] 
 
 
 
-modelrg<-'add=~NA*Autism + start(0.4)*ADHD
-  aut=~NA*Autism
-  LT=~NA*T
-  add~~1*add
-  aut~~1*aut
-  LT~~1*LT
-  Autism~~0*Autism
-  ADHD~~0*ADHD
-  T~~0*T
-  add~~0*aut
-  Autism~~0*ADHD
-  Autism~~0*T
-  ADHD~~0*T
-  '
-output<-usermodel(LDSCoutput,estimation="DWLS",model=modelrg)
-output
+setwd("/mnt/beegfs/home4/arc/vw260/ldsc/sumstats")
+traits <- c("dep_anx1.sumstats.gz", "dep_anx2.sumstats.gz", "dep_anx3.sumstats.gz")
+sample.prev <- c(NA, NA, NA)
+population.prev <- c(NA, NA, NA)
+ld <- "~/ldsc/eur_w_ld_chr/"
+wld <- "~/ldsc/eur_w_ld_chr/"
+trait.names<-c("depression",  "anxiety", "comorbid")
+LDSCoutput <- ldsc(traits, sample.prev, population.prev, ld, wld, trait.names)
+
+
+GWISmodel <- 'comorbid ~ depression + anxiety
+depression~~anxiety'
+
+#run the model using the usermodel function
+GWISoutput<-usermodel(LDSCoutput, estimation = "DWLS", model = GWISmodel, CFIcalc = TRUE, std.lv = TRUE, imp_cov = FALSE)
+
+#print the output
+GWISoutput
