3e441cc16359ec186324002cdda7c53b7c783944
Varun Warrier <vw260@medschl.cam.ac.uk>
2025-01-03 06:51:20 +0000

    Update genomicSEM_twofactor.R

--- gSEM/genomicSEM_twofactor.R
+++ gSEM/genomicSEM_twofactor.R
@@ -86,15 +86,15 @@ Autism_CFA<-usermodel(LDSCoutput, estimation = "DWLS", model = CFAofEFA, CFIcalc
 Autism_CFA 
 
 
-#####New - with the SPARK meta-analysed GWAS - just change the sumstats as follows and rerun everything above
+#####New - with the SPARK meta-analysed GWAS - just change the sumstats as follows and rerun everything above after tweaking it
 
 setwd("/mnt/beegfs/home4/arc/vw260/ldsc/sumstats")
-traits <- c("Autism_PGC2_2017.sumstats.gz", "Autism_iPSYCH_under9.sumstats.gz", "Autism_iPSYCH_over12.sumstats.gz", "Finngen_r10_autism.sumstats.gz",  "SPARK_under6_meta.sumstats", "SPARK_over10_meta.sumstats")
+traits <- c("Autism_PGC2_2017.sumstats.gz", "Autism_iPSYCH_under9.sumstats.gz", "autism_iPSYCH_over10.sumstats.gz", "Finngen_r10_autism.sumstats.gz",  "SPARK_under6_meta.sumstats", "SPARK_over10_meta.sumstats")
 sample.prev <- c(NA, NA, NA, NA, NA, NA)
 population.prev <- c(NA, NA, NA, NA, NA, NA)
 ld <- "~/ldsc/eur_w_ld_chr/"
 wld <- "~/ldsc/eur_w_ld_chr/"
-trait.names<-c("PGC",  "iPSYCH_under9", "iPSYCH_over12", "FinnGen",  "SPARK_under6",  "SPARK_over10")
+trait.names<-c("PGC",  "iPSYCH_under9", "iPSYCH_over10", "FinnGen",  "SPARK_under6",  "SPARK_over10")
 LDSCoutput <- ldsc(traits, sample.prev, population.prev, ld, wld, trait.names)
 ##optional command to save the ldsc output in case you want to use it in a later R session. 
 save(LDSCoutput, file="Autism_all_nonoverlapping_GWAS_withSPARK_extremevalue_meta.RData")
